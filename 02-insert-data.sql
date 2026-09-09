51fa596fd427048ca08b43de00b440871b73fcb4
Fahim sahriar <fahimsahriar2002@gmail.com>
2026-09-09 21:37:42 +0600

    insert data

---
+++ 02-insert-data.sql
@@ -0,0 +1,7 @@
+USE practice_db;
+
+INSERT INTO students (name, age, email)
+VALUES('Rahim',22,'rahim@example.com');
+
+INSERT INTO students (name,age,email)
+VALUES('sakib',23,'sakib@example.com');
